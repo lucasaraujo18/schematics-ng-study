756c574317175f47abc2b1751220e09bb4949e56
lucas.florencio <lucas.florencio@extreme.digital>
2025-03-30 15:52:43 -0300

    wip

--- schematics-study/src/schematics-study/schematics-dialog/index.ts
+++ schematics-study/src/schematics-study/schematics-dialog/index.ts
@@ -1,10 +1,21 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
+import { Rule, apply, template, mergeWith, url } from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function schematicsDialog(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+export function schematicsDialog(options: any): Rule {
+  return () => {
+    if (!options.className) {
+      options.className = strings.classify(options.name) + 'DialogComponent';
+    }
+
+    const sourceTemplates = apply(url('./files'), [
+      template({
+        ...options,
+        classify: strings.classify,
+      }),
+    ]);
+
+    return mergeWith(sourceTemplates);
   };
 }
