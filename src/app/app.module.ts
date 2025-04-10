a84584717c402d8d16eb370c22f7d64eb2e36163
lucas.florencio <lucas.florencio@extreme.digital>
2025-04-10 12:02:27 -0300

    dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ConfirmacaoComponent } from './components/confirmacao.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
@@ -12,7 +11,6 @@ import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent,
-    ConfirmacaoComponent
   ],
   imports: [
     BrowserModule,
